92aae97dae87948c7c50915dffdd01c008e4b416
Denis <kripmrx@gmail.com>
2025-11-09 14:20:49 +0500

    create element logic

---
+++ reactFS/createElement.js
@@ -0,0 +1,19 @@
+function creacteElement(type, props, ...children) {
+    return {
+        type: type,
+        props: {
+            ...props,
+            children,
+        },
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
